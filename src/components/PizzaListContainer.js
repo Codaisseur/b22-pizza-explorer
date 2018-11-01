96ab82f5fd8a2a7741850203b1ad7143371ddf96
Wouter de Vos <wouter@codaisseur.com>
2018-11-01 17:28:36 +0100

    Pizza explorer

---
+++ src/components/PizzaListContainer.js
@@ -0,0 +1,20 @@
+import * as React from 'react'
+import PizzaList from './PizzaList'
+import { connect } from 'react-redux'
+
+class PizzaListContainer extends React.Component {
+  selectPizza = (id) => {
+    this.props.dispatch({
+      type: 'SELECT_PIZZA',
+      payload: id
+    })
+  }
+
+  render() {
+    return <PizzaList pizzas={this.props.pizzas} selectPizza={this.selectPizza} />
+  }
+}
+
+const mapStateToProps = ({ pizzas }) => ({ pizzas })
+
+export default connect(mapStateToProps)(PizzaListContainer)
